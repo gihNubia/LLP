d53a676d67216afd832a56bf053fd5408b7f63a4
gihNubia <92544436+gihNubia@users.noreply.github.com>
2023-04-25 22:01:58 -0300

    Add files via upload

---
+++ 1.1/interpreter/expr/UnaryExpr.java
@@ -0,0 +1,82 @@
+package interpreter.expr;
+
+import interpreter.value.BoolValue;
+import interpreter.value.NumberValue;
+import interpreter.value.Value;
+
+public class UnaryExpr extends Expr {
+    
+    public static enum Op {
+        NotOp,
+        PosOp,
+        NegOp,
+        PreInc,
+        PosInc,
+        PreDec,
+        PosDec
+    }
+
+    private Expr expr;
+    private Op op;
+
+    public UnaryExpr(int line, Expr expr, Op op) {
+        super(line);
+        this.expr = expr;
+        this.op = op;
+    }
+
+    @Override
+    public Value<?> expr() {
+        Value<?> v = expr.expr();
+
+        switch (this.op) {
+            case NotOp:
+                return notOp(v);
+            case PosOp:
+                return posOp(v);
+            case NegOp:
+                return negOp(v);
+            case PreInc:
+                return preIncOp(v);
+            case PosInc:
+                return posIncOp(v);
+            case PreDec:
+                return preDecOp(v);
+            case PosDec:
+            default:
+                return posDecOp(v);
+        }
+    }
+
+    private Value<?> notOp(Value<?> v) {
+        boolean b = BoolValue.convert(v);
+        return new BoolValue(!b);
+    }
+
+    private Value<?> posOp(Value<?> v) {
+        double d = NumberValue.convert(v);
+        return new NumberValue(d);
+    }
+
+    private Value<?> negOp(Value<?> v) {
+        double d = NumberValue.convert(v);
+        return new NumberValue(-d);
+    }
+
+    private Value<?> preIncOp(Value<?> v) {
+        throw new RuntimeException("implement me!");
+    }
+
+    private Value<?> posIncOp(Value<?> v) {
+        throw new RuntimeException("implement me!");
+    }
+
+    private Value<?> preDecOp(Value<?> v) {
+        throw new RuntimeException("implement me!");
+    }
+
+    private Value<?> posDecOp(Value<?> v) {
+        throw new RuntimeException("implement me!");
+    }
+
+}

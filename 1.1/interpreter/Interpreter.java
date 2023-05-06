d0da63bd2ef4f78d883f71a5cef785576e5b2aa8
Erick Rocha <erickfr1011@gmail.com>
2023-05-05 23:20:08 -0300

    Implementar funcoes nativas

--- 1.1/interpreter/Interpreter.java
+++ 1.1/interpreter/Interpreter.java
@@ -1,8 +1,19 @@
 package interpreter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import interpreter.command.Command;
+import interpreter.expr.ConstExpr;
 import interpreter.expr.Expr;
+import interpreter.expr.Variable;
+import interpreter.function.NativeFunction;
+import interpreter.function.NativeOp;
+import interpreter.value.FunctionValue;
+import interpreter.value.ObjectValue;
+import interpreter.value.TextValue;
 import interpreter.value.Value;
+import lexical.Token;
 
 public class Interpreter {
 
@@ -10,6 +21,45 @@ public class Interpreter {
 
     static {
         globals = new Environment();
+
+        Map<TextValue, Value<?>> mp = new HashMap<TextValue, Value<?>>();
+
+        // Criando funcao Log
+        Environment envLog = new Environment(globals);
+        Variable paramsLog = envLog.declare(
+                    new Token("params", Token.Type.NAME, null),
+                    false);
+        NativeFunction nfLog = new NativeFunction(paramsLog, NativeOp.Log);
+        FunctionValue fvLog = new FunctionValue(nfLog);
+        TextValue tvLog = new TextValue("log");
+        mp.put(tvLog, fvLog);
+
+        // Criando funcao Read
+        Environment envRead = new Environment(globals);
+        Variable paramsRead = envRead.declare(
+                    new Token("params", Token.Type.NAME, null),
+                    false);
+        NativeFunction nfRead = new NativeFunction(paramsRead, NativeOp.Read);
+        FunctionValue fvRead = new FunctionValue(nfRead);
+        TextValue tvRead = new TextValue("read");
+        mp.put(tvRead, fvRead);
+
+        // Criando funcao Random
+        Environment envRandom = new Environment(globals);
+        Variable paramsRandom = envRandom.declare(
+                    new Token("params", Token.Type.NAME, null),
+                    false);
+        NativeFunction nfRandom = new NativeFunction(paramsRandom, NativeOp.Random);
+        FunctionValue fvRandom = new FunctionValue(nfRandom);
+        TextValue tvRandom = new TextValue("random");
+        mp.put(tvRandom, fvRandom);
+
+
+        ObjectValue ov = new ObjectValue(mp);
+        Variable console = globals.declare(
+            new Token("console", Token.Type.NAME, null),
+            false);
+        console.setValue(ov);
     }
 
     private Interpreter() {
